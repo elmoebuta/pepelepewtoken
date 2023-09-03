e26fe352b9f3946a5d0fce018084156f58965dab
elmoebuta <u201726329@upc.edu.pe>
2023-09-03 01:14:37 -0500

    primer commi
    primer commi

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,16 @@
-import { ThirdwebProvider } from '@thirdweb-dev/react';
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import '../styles/globals.css';
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = 'ethereum';
+const activeChainId = ChainId.Mainnet;
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<ThirdwebProvider
-			activeChain={activeChain}
-			clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-		>
-			<Component {...pageProps} />
-		</ThirdwebProvider>
+		<ThirdwebProvider desiredChainId={activeChainId}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
 	);
 }
 
